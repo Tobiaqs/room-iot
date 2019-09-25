6d4d3e812e6262916d1e7f84cc3d681b74d9917b
Tobias Sijtsma <tobias@tobiass.nl>
2019-09-26 00:38:56 +0200

    update

--- get_colors.py
+++ get_colors.py
@@ -3,7 +3,7 @@ from netaddr import EUI
 from broadlink import rm
 from binascii import hexlify
 
-RM3Device = rm(("88.159.235.52", 60000), EUI("60-E3-27-4B-69-23"))
+RM3Device = rm(("192.168.178.130", 80), EUI("B4:43:0D:FC:05:CE"), 0x27c2)
 RM3Device.auth()
 
 RM3Device.enter_learning()
@@ -14,7 +14,7 @@ while True:
     learned_cmd = RM3Device.check_data()
     if learned_cmd is not None:
         cmd = str(hexlify(learned_cmd), 'ascii')
-        name = input("How'd you name this color?\n")
+        name = input("How'd you name this command?\n")
         if name == "done":
             print()
             print()
